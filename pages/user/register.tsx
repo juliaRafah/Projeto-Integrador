db13340b3ebf09c390dbda7e5a34306798d8527e
juliaRafah <168195740+juliaRafah@users.noreply.github.com>
2024-08-30 11:28:48 -0300

    30/08/2024

--- pages/user/register.tsx
+++ pages/user/register.tsx
@@ -1,7 +1,17 @@
+import { Head } from "next/document";
+
+
 export default function registerPage (){
     return (
         <main className='flex min-h-screen flex-col items-center justify-between p-24'>
         
+        <Head>
+            <title> Cadastro </title>
+        </Head>
+
+            <div>
+                <input type="text" placeholder="Name" onChange={(event) => {(   'name')}}/>
+            </div>
         </main>
-    )
+    );
 }
